a0e12def591a2312a0e4acb1fd012e6c86e41b94
Ariemaharrucha <arie.maharucha.zakka@gmail.com>
2025-01-08 08:35:23 +0700

    add component search location

---
+++ client/src/features/prompt-page/components/LocationForm.tsx
@@ -0,0 +1,71 @@
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import { autoComplete } from "@/lib/google";
+import { useEffect, useState } from "react";
+
+export const LocationForm = () => {
+  const [predictions, setPredictions] = useState([]);
+  const [input, setInput] = useState("");
+  useEffect(() => {
+    const fetchPredictios = async () => {
+      const predictions = await autoComplete(input);
+      console.log(predictions);
+      setPredictions(predictions ?? []);
+    };
+    fetchPredictios();
+  }, [input]);
+  return (
+    <section className=" w-2/3 m-auto ">
+      <div className="text-center">
+        <h1 className="mt-10 mb-10 text-4xl font-bold ">
+          Pertama, ke mana kau ingin pergi?
+        </h1>
+        <p className="text-slate-400 text-md pb-12">
+          Anda akan mendapatkan res ulang untuk rencana perjalanan
+        </p>
+        <Command
+          className="rounded-lg border shadow-md md:min-w-[450px]"
+          value={input}
+          onValueChange={setInput}
+        >
+          <CommandInput placeholder="Type a command or search..." />
+          <CommandList>
+            <CommandEmpty>No results found.</CommandEmpty>
+            {/* <CommandGroup heading="Suggestions">
+            {predictions.map((prediction) => (
+              <CommandItem key={prediction.id}>
+                {prediction.address.label}
+              </CommandItem>
+            ))}
+          </CommandGroup> */}
+          </CommandList>
+        </Command>
+      </div>
+
+      <div className="mt-20">
+        <h1 className="text-center text-3xl">
+          Atau memulai dengan tujuan populer
+        </h1>
+        <div className="mt-8">
+          {/* card */}
+          <div>
+            <div className="overflow-hidden size-32 rounded-md">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/1/10/Yogyakarta_Indonesia_Tugu-Yogyakarta-02.jpg"
+                alt=""
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <h3 className="mt-2 font-bold">Yogyakarta</h3>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
