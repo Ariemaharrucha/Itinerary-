8ee370f5cee5ca7c17790b367b406d04ea9edd2c
Ariemaharrucha <arie.maharucha.zakka@gmail.com>
2025-01-29 14:55:52 +0700

    add more interest

--- src/features/prompt-page/components/Interested.tsx
+++ src/features/prompt-page/components/Interested.tsx
@@ -1,9 +1,43 @@
+import { Plus } from "lucide-react";
+import { Check } from "lucide-react";
 import { useInterested } from "../hooks/useInterested";
 import { WizardNavigation } from "./WizardNavigation";
 import { vacationInterests } from "@/constant/interests";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useEffect } from "react";
+// import { Button } from "@/components/ui/button";
 
 export const Interested = () => {
-  const {selectedInterests, isLoading, isError, isValid, handleTogglePreferences, handleSubmit} = useInterested();
+  const {
+    selectedInterests,
+    newInterests,
+    setNewInterests,
+    isLoading,
+    isError,
+    isValid,
+    handleTogglePreferences,
+    handleSubmit,
+    handleNewInterests,
+    // addNewInterests,
+  } = useInterested();
+
+  useEffect(() => {
+    const savedNewInterests = localStorage.getItem("newInterests");
+
+    if (savedNewInterests) {
+      setNewInterests(savedNewInterests);
+    }
+    console.log(savedNewInterests);
+  }, [setNewInterests]);
 
   return (
     <section className=" w-2/3 m-auto ">
@@ -30,10 +64,57 @@ export const Interested = () => {
             <span>{interest.label}</span>
           </label>
         ))}
+
+        <Dialog>
+          <DialogTrigger asChild>
+            <button
+              className={`inline-flex items-center gap-2 rounded-full border px-4 py-4 text-base font-semibold hover:bg-[#34e0a1] ${
+                newInterests.trim() ? "bg-[#34e0a1]" : "bg-transparent"
+              } cursor-pointer text-black`}
+            >
+              {newInterests.trim() ? (
+                <>
+                  <Check /> add interest
+                </>
+              ) : (
+                <>
+                  <Plus /> add interest
+                </>
+              )}
+            </button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Tambah yang kamu minati</DialogTitle>
+            </DialogHeader>
+            <DialogDescription>
+              <strong>Example : </strong>
+              <span>kuliner, gunung</span>
+            </DialogDescription>
+            <textarea
+              className="p-2 border-2 border-black"
+              rows={6}
+              value={newInterests}
+              onChange={handleNewInterests}
+            ></textarea>
+            <DialogFooter>
+              <DialogClose asChild>
+                {/* <Button type="button" onClick={addNewInterests}>
+                  Tambah
+                </Button> */}
+              </DialogClose>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </div>
       {isError && <p className="text-red-500 text-center mt-4">{isError}</p>}
-      {isLoading ? (<p className="text-xl text-slate-400 text-center mt-3 font-semibold animate-pulse">Please wait...</p>) :
-      (<WizardNavigation onSubmit={handleSubmit} isValid={isValid} />)}
+      {isLoading ? (
+        <p className="text-xl text-slate-400 text-center mt-3 font-semibold animate-pulse">
+          Please wait...
+        </p>
+      ) : (
+        <WizardNavigation onSubmit={handleSubmit} isValid={isValid} />
+      )}
     </section>
   );
 };
--- src/features/prompt-page/hooks/useInterested.tsx
+++ src/features/prompt-page/hooks/useInterested.tsx
@@ -1,13 +1,15 @@
 import useFormState from "@/store/useStore";
-import { useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { generateItenray } from "../services/get.itinerary";
+import { vacationInterests } from "@/constant/interests";
 
 export const useInterested = () => {
   const { stepData, setStepData, resetForm, setTravelPlan } = useFormState();
   const [selectedInterests, setSelectedInterests] = useState<string[]>(
     Array.isArray(stepData?.preferences) ? stepData.preferences : []
   );
+  const [newInterests, setNewInterests] = useState<string>("");
   const [isLoading, setLoading] = useState<boolean>(false);
   const [isError, setError] = useState<string>("");
   const navigate = useNavigate();
@@ -26,6 +28,29 @@ export const useInterested = () => {
     setStepData("preferences", updatedInterests);
   };
 
+  const handleNewInterests = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const value = e.target.value.toLowerCase();
+    setNewInterests(value);
+    
+    localStorage.setItem("newInterests", value);
+    // Proses hanya nilai terakhir (setelah koma)
+    const lastInterest = value.split(",").pop()?.trim() ?? "";
+  
+    // Pastikan nilai terakhir tidak ada di `vacationInterests` dan `selectedInterests`
+    const isAlreadyInVacationInterests = vacationInterests.some(
+      (interest) => interest.value.toLowerCase() === lastInterest
+    );
+    const isDuplicate = selectedInterests.includes(lastInterest);
+  
+    if (lastInterest.length > 1 && !isDuplicate && !isAlreadyInVacationInterests) {
+      const updatedInterests = [...new Set([...selectedInterests, lastInterest])];
+      setSelectedInterests(updatedInterests);
+      setStepData("preferences", updatedInterests);
+    }
+  
+    // console.log(lastInterest);
+  };
+
   const handleSubmit = async () => {
     try {
       setLoading(true);
@@ -36,6 +61,7 @@ export const useInterested = () => {
         navigate("/itinerary-preview");
         resetForm();
         setSelectedInterests([]);
+        setNewInterests("");
       } else {
         setError("Failed to generate itinerary. Please try again.");
       }
@@ -45,5 +71,15 @@ export const useInterested = () => {
       setLoading(false);
     }
   };
-  return {selectedInterests, isLoading, isError, isValid, handleTogglePreferences, handleSubmit};
+  return {
+    selectedInterests,
+    newInterests,
+    setNewInterests,
+    isLoading,
+    isError,
+    isValid,
+    handleTogglePreferences,
+    handleSubmit,
+    handleNewInterests,
+  };
 };
